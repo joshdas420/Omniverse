05cdd857c941997c7d1cae71008ae052887999f0
joshdas420 <joshdunmoredas@gmail.com>
2025-11-07 11:45:22 +0800

    initalizing files

---
+++ target/debug/deps/my_rust_project-01981994db946981.d
@@ -0,0 +1,5 @@
+/Users/joshdunmoredas/Desktop/coding stuff/blockchain rust project/Omniverse-1/target/debug/deps/my_rust_project-01981994db946981.d: src/main.rs
+
+/Users/joshdunmoredas/Desktop/coding stuff/blockchain rust project/Omniverse-1/target/debug/deps/libmy_rust_project-01981994db946981.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/my_rust_project-5cc61ef35b56751a.d
@@ -0,0 +1,5 @@
+/Users/joshdunmoredas/Desktop/coding stuff/blockchain rust project/Omniverse-1/target/debug/deps/my_rust_project-5cc61ef35b56751a.d: src/main.rs
+
+/Users/joshdunmoredas/Desktop/coding stuff/blockchain rust project/Omniverse-1/target/debug/deps/libmy_rust_project-5cc61ef35b56751a.rmeta: src/main.rs
+
+src/main.rs:
